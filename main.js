079e121d7464cbe6a297b75c3744ef646d6a3797
Thomasnogaroto <163418456+Thomasnogaroto@users.noreply.github.com>
2024-03-15 10:07:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://img.br.my-best.com/product_images/124c9a1d45f98483f08099c8588ab83a.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=240&h=240&fit=fill&fill=solid&fill-color=FFFFFF&s=7a485b8cd7dc3d450e4808ca645c0a70"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://http2.mlstatic.com/D_NQ_NP_699081-MLB49851696425_052022-O.webp"
+}
+
